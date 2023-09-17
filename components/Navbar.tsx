073f13214a8b0ab9de32b7e748c77292e2114442
ivan <ivan.correa24@yahoo.com>
2023-09-17 18:52:30 -0300

    first commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div className="sticky bg-red-500 flex items-center justify-center">
+      aaa
+      <div className="bg-yellow-500">eee</div>
+    </div>
+  );
+};
+
+export default Navbar;
