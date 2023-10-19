802a7f49d0ea6aa30b9ae1af0d4151790459aa3c
ivan correa <ivan.correa24@yahoo.com>
2023-10-19 07:19:16 -0300

    creating develop branch

---
+++ next.js-frontend/src/types/TasksItems.types.ts
@@ -0,0 +1,23 @@
+export interface TaskItemProps {
+  task: Task;
+}
+export type ShowDetails = {
+  [taskId: string]: boolean;
+};
+
+export type Task = {
+  id: string;
+  title: string;
+  description: string;
+  state: 'PENDING' | 'IN_PROGRESS' | 'DONE';
+};
+
+export type UseInputType = {
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  value?: string;
+};
+
+export type createTaskDto = {
+  title: string;
+  description: string;
+};
