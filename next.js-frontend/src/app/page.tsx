4b0b2269cb05f6b84f9653f450c92945177e2da4
ivan correa <ivan.correa24@yahoo.com>
2023-10-19 04:45:36 -0300

    creating develop branch

---
+++ next.js-frontend/src/app/page.tsx
@@ -0,0 +1,10 @@
+import List from '../views/List';
+import Navbar from '@/components/Navbar';
+export default function Home() {
+  return (
+    <div>
+      {/* <Navbar /> */}
+      <List />
+    </div>
+  );
+}
---
+++ next.js-frontend/src/services/task.service.ts
@@ -0,0 +1,54 @@
+import { Task } from '@/types/TasksItems.types';
+import axios from 'axios';
+
+const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+
+class Tasks_Services {
+  private static instace: Tasks_Services | null = null;
+
+  static getInstance(): Tasks_Services {
+    if (!Tasks_Services.instace) {
+      Tasks_Services.instace = new Tasks_Services();
+    }
+    return Tasks_Services.instace;
+  }
+
+  async getAllTasks() {
+    try {
+      const response = await axios.get(`${apiUrl}`, {
+        withCredentials: true,
+      });
+      return response.data;
+    } catch (error) {
+      console.log('get all tasks error : ', error);
+    }
+  }
+
+  async updateTaskStatus(taskId: string, newStatus: string) {
+    try {
+      await axios.patch(
+        `${apiUrl}/${taskId}`,
+        { state: newStatus },
+        {
+          withCredentials: true,
+        }
+      );
+      return ' task status updated succesfully ';
+    } catch (error: any) {
+      console.log('update task error : ', error.response.data.message);
+    }
+  }
+
+  async updateTask(taskId: string, updatedTasks: Task) {
+    try {
+      await axios.put(`${apiUrl}/${taskId}`, updatedTasks, {
+        withCredentials: true,
+      });
+      return 'task updated succesfully';
+    } catch (error: any) {
+      console.log('update task error : ', error.response.data.message);
+    }
+  }
+}
+
+export default Tasks_Services.getInstance();
