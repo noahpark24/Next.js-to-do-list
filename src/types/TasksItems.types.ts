e383b2428782c2579471eff19568622a6fc30887
ivan correa <ivan.correa24@yahoo.com>
2023-10-19 04:11:35 -0300

    feat: functional to do list

---
+++ src/types/TasksItems.types.ts
@@ -0,0 +1,18 @@
+export interface TaskItemProps {
+  task: Task;
+}
+export type ShowDetails = {
+  [taskId: string]: boolean;
+};
+
+export type Task = {
+  id: string;
+  title: string;
+  description: string;
+  state: 'PENDING' | 'IN_PROGRESS' | 'DONE';
+};
+
+export type UseInputType = {
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  value?: string;
+};
